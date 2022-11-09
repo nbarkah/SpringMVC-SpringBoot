60b25de8d525c0ebf054f5b3a1ba547dab67caae
nbarkah <nurhaibarkah9@gmail.com>
2022-11-09 13:22:13 +0700

    SpringMVC dan SpringBoot

--- MiniProjectPerpus/src/main/java/com/perpus/dao/PersonDao.java
+++
@@ -1,9 +0,0 @@
-package com.perpus.dao;
-
-import com.perpus.model.Person;
-
-public interface PersonDao {
-	public void registerPerson(Person person);
-
-	public Person loginPerson(Person person);
-}
--- MiniProjectPerpus/src/main/java/com/perpus/service/PersonService.java
+++
@@ -1,9 +0,0 @@
-package com.perpus.service;
-
-import com.perpus.model.Person;
-
-public interface PersonService {
-	public void registerPerson(Person person);
-
-	public Person loginPerson(Person person);
-}
